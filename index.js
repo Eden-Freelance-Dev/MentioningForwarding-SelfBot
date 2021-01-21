b7a3d87049931768b96db1a75a30e487cb5d4f70
Eden <edencheung5112006@gmail.com>
2021-01-21 12:08:34 +0800

    Changed to use txt for channels

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client } = require("discord.js");
+const fs = require('fs');
 
-const { WATCHING_TOKEN, FORWARDING_TOKEN, WATCHING_CHANNELS } = require('./config.json');
+const { WATCHING_TOKEN, FORWARDING_TOKEN } = require('./config.json');
 
 const watcher = new Client();
 const forwarder = new Client();
@@ -8,14 +9,23 @@ const forwarder = new Client();
 watcher.login(WATCHING_TOKEN);
 forwarder.login(FORWARDING_TOKEN);
 
+const watchingChannels = fs.readFileSync('channels.txt', 'utf-8').split('\n').map(str => {
+    return {
+        watching: str.trim().split(' ')[0],
+        forwarding: str.trim().split(' ')[1]
+    }
+});
+
+console.log(watchingChannels);
+
 watcher.on('ready', async() => {
     console.log(`${watcher.user.username} is ready!`);
-    WATCHING_CHANNELS.forEach(element => {
-        const channel = watcher.channels.get(element.watching);
+    watchingChannels.forEach(channelPair => {
+        const channel = watcher.channels.get(channelPair.watching);
         const collector = channel.createMessageCollector(() => true, {});
 
         collector.on('collect', async m => {
-            const target = forwarder.channels.get(element.forwarding);
+            const target = forwarder.channels.get(channelPair.forwarding);
             if(m.content != ''){
                 await target.send(m.content);
             }
